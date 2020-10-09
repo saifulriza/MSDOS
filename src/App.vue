7fa04a56d28339725df1f5c5829ca514d1dd1560
saifulriza123 <mail.saifulriza@gmail.com>
2020-10-09 10:47:11 +0700

    MSDOS init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="tui-screen-800-600 black white-text container"
+    style="padding: 2px; margin-top: 30px"
+  >
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss" src="./assets/scss/tuicss.scss">
+</style>
---
+++ src/views/%F0%9F%98%83.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>hai ssss</h1>
+</template>
+<script>
+import { onMounted, ref } from "vue";
+export default {
+  setup() {
+    let second = ref("");
+
+    function formatTime() {
+      window.location.hash = new window.Date();
+    }
+    // function loop() {
+    //   let i,
+    //     n,
+    //     s = "";
+    //   for (i = 0; i < 10; i++) {
+    //     n = Math.floor(Math.sin(Date.now() / 200 + i / 2) * 4) + 4;
+    //     s += String.fromCharCode(0x2581 + n);
+    //   }
+    //   window.location.hash = s;
+    //   setTimeout(loop, 1000);
+    // }
+
+    onMounted(() => {
+      setInterval(() => {
+        formatTime();
+      }, 1000);
+    });
+  },
+};
+</script>
